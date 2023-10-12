6c3d5fa5b34f4b526ed204f3db33e4843aec6591
cswong235 <131174999+cswong235@users.noreply.github.com>
2023-10-12 20:33:56 +1100

    Added validation to product addition form

--- addProduct.php
+++ addProduct.php
@@ -20,30 +20,137 @@
                 <a href="searchProduct.php">Manage Product Records</a>
             </nav>
         </div>
+        <?php
+
+        $product_name = $product_stock = $product_supply_date = $product_supplier = $product_price_per_unit = "";
+        $product_name_error = $product_stock_error = $product_supply_date_error = $product_supplier_error = $product_price_per_unit_error = "";
+        $verify_name = $verify_stock = $verify_supply_date = $verify_supplier = $verify_price_per_unit = 0;
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            if (empty($_POST["product_name"])) {
+                $product_name_error = "Product name is required";
+            } else {
+                $product_name = $_POST["product_name"];
+                $verify_name = 1;
+                if (strlen($product_name) > 50) {
+                    $product_name_error = "The entered product name cannot exceed 50 characters. Please try again.";
+                }
+            }
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            if (empty($_POST["product_stock"])) {
+                $product_stock_error = "Stock quantity is required";
+            } else {
+                $product_stock = $_POST["product_stock"];
+                $verify_stock = 1;
+            }
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            if (empty($_POST["product_supply_date"])) {
+                $product_supply_date_error = "Supply date is required";
+            } else {
+                $product_supply_date = $_POST["product_supply_date"];
+                $verify_supply_date = 1;
+            }
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            if (empty($_POST["product_supplier"])) {
+                $product_supplier_error = "Product supplier is required";
+            } else {
+                $product_supplier = $_POST["product_supplier"];
+                $verify_supplier = 1;
+                if (strlen($product_supplier) > 50) {
+                    $product_supplier_error = "The entered product supplier cannot exceed 50 characters. Please try again.";
+                }
+            }
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            if (empty($_POST["product_price_per_unit"])) {
+                $product_price_per_unit_error = "The price per unit of the product is required";
+            } else {
+                $product_price_per_unit = $_POST["product_price_per_unit"];
+                $verify_price_per_unit = 1;
+            }
+        }
+
+        ?>
         <h1 class="center">Add a new product record</h1>
-        <form id="product_R" method="post" action="product_process.php">
+        <div id="container">
+        <form method="post" id="add_product" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
             <fieldset>
                 <legend>Add Product Form</legend>
                 <p>
                     <label for="product_name">Product Name:</label>
-                    <input type="text" name="product_name" id="product_name" maxlength="40" size="20" required="required" placeholder="Product Name" />
+                    <input type="text" name="product_name" id="product_name" maxlength="50" size="50" placeholder="Product Name" value="<?php echo $product_name;?>"/>
+                    <span class="error">* <?php echo $product_name_error;?> </span>
                     <br><br>
                     <label for="product_stock">Product Stock:</label>
-                    <input type="number" name="product_stock" id="product_stock" maxlength="40" size="20" required="required" placeholder="Product Stock" />
+                    <input type="number" name="product_stock" id="product_stock" maxlength="40" size="20" placeholder="Product Stock" value="<?php echo $product_stock;?>"/>
+                    <span class="error">* <?php echo $product_stock_error;?> </span>
                     <br><br>
                     <label for="product_supply_date">Product Supply Date:</label>
-                    <input type="date" name="product_supply_date" id="product_supply_date" maxlength="40" size="20" required="required" placeholder="Product Supply Date" />
+                    <input type="date" name="product_supply_date" id="product_supply_date" maxlength="40" size="20" placeholder="Product Supply Date" value="<?php echo $product_supply_date;?>"/>
+                    <span class="error">* <?php echo $product_supply_date_error;?> </span>
                     <br><br>
                     <label for="product_supplier">Product Supplier:</label>
-                    <input type="text" name="product_supplier" id="product_supplier" maxlength="40" size="20" required="required" placeholder="Product Supplier" />
+                    <input type="text" name="product_supplier" id="product_supplier" maxlength="50" size="50" placeholder="Product Supplier" value="<?php echo $product_supplier;?>"/>
+                    <span class="error">* <?php echo $product_supplier_error;?> </span>
                     <br><br>
                     <label for="product_price_per_unit">Product Price Per Unit:</label>
-                    $<input type="number" step="0.01" name="product_price_per_unit" id="product_price_per_unit" maxlength="40" size="20" required="required" placeholder="Product Price Per Unit" />
+                    $<input type="number" step="0.01" name="product_price_per_unit" id="product_price_per_unit" maxlength="40" size="20" placeholder="Product Price Per Unit" value="<?php echo $product_price_per_unit;?>"/>
+                    <span class="error">* <?php echo $product_price_per_unit_error;?> </span>
                     <br><br>
                 </p>
-            <input type="submit" value="Submit" />
+            <input type="submit" name="submit" class="button"></input>
             <input type="reset" value="Reset Form" />
         </form>
+        <?php
+
+        function dupeProductName($product_name){
+            $conn = mysqli_connect('localhost','root','','gotogro-mrm');
+            if(!$conn){
+                echo "Can't connect to database.";
+            }
+            $product_name = mysqli_real_escape_string($conn, $product_name);
+            $sql_product_name = "SELECT * FROM product WHERE ProductName = '$product_name'";
+            $result = mysqli_query($conn, $sql_product_name);
+            if (mysqli_num_rows($result) == 0){
+                return true;
+            }else{
+            return false;
+            }
+        }
+
+        if($verify_name = $verify_stock = $verify_supply_date = $verify_supplier = $verify_price_per_unit == 1){
+            if (dupeProductName($_POST['product_name'])) {
+                $conn = mysqli_connect('localhost', 'root', '', 'gotogro-mrm');
+                if (!$conn) {
+                    echo "Can't connect to database.";
+                }
+                $SQL = "INSERT INTO product (ProductName, ProductStock, ProductSupplyDate, ProductSupplier, ProductPricePerUnit) 
+                VALUES ('$product_name', '$product_stock', '$product_supply_date', '$product_supplier', '$product_price_per_unit')";
+
+                $run = mysqli_query($conn, $SQL) or die(mysqli_error($conn));
+                if($run){?>
+                    <script type="text/javascript">
+                        alert("Your submission is successful.");
+                        window.location.href = "product.php";
+                    </script>
+                        <?php
+                }
+            } else {
+                ?>
+                    <script type="text/javascript">
+                        alert("Duplicate product name detected. Please try again.");
+                    </script>
+                <?php
+            }
+        }
+        ?>
         <hr />
         <p>This section will update depending on if its an existing product and the given values.</p>
         <h3>Stock on hand:</h3>
@@ -64,9 +171,6 @@
 
             mysqli_close($conn);
         ?>
-        <hr/>
-        <h3>Product incoming</h3>
-        <p> ------ </p>
-        <hr />
+        </div>
     </body>
 </html>
